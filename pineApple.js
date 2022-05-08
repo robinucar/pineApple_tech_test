e16f6772a959bab4a53a7a45a63db9e02af8cab9
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-05-08 23:22:55 +0100

    print pineapple instead of number if number is multiple of both 3 and 5

--- pineApple.js
+++ pineApple.js
@@ -1,10 +1,18 @@
 const pineApple = (start_num, end_num) => {
- const numbers = []
- for(let num = start_num; num <= end_num; num++) {
-     numbers.push(num)
- }
- return numbers
-}
+  const numbers = [];
+  for (let num = start_num; num <= end_num; num++) {
+    if (num % 3 === 0 && num % 5 === 0) {
+      numbers.push("PineApple");
+    } else if (num % 3 === 0) {
+      numbers.push("Pine");
+    } else if (num % 5 === 0) {
+      numbers.push("Apple");
+    } else {
+      numbers.push(num);
+    }
+  }
+  return numbers;
+};
 
-console.log(pineApple(1,100))
-module.exports = pineApple
+console.log(pineApple(1, 100));
+module.exports = pineApple;
--- pineApple.test.js
+++ pineApple.test.js
@@ -3,4 +3,16 @@ describe('pineApple', () => {
     it('returns numbers from 1 to 100', () => {
         expect(pineApple(1,100).length).toEqual(100)
     })
+
+    it('returns "Pine" instead of the number if number is multiple of 3', () => {
+        expect(pineApple(1,100)).toContain('Pine')
+    })
+
+    it('reurns Apple instead of number if number is multiple of 5', () => {
+        expect(pineApple(1,100)).toContain('Apple')
+    })
+
+    it('returns PineApple instead of number if number is multiple of both 3 and 5',() => {
+        expect(pineApple(1,100)).toContain('PineApple')
+    })
 })
