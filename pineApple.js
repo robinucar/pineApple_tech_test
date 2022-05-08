1f8a309764900ae245e8b81bb6efbd452bbd81cf
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-05-08 23:13:02 +0100

    refactoring parametrs added

--- pineApple.js
+++ pineApple.js
@@ -1,10 +1,10 @@
-const pineApple = () => {
+const pineApple = (start_num, end_num) => {
  const numbers = []
- for(let num = 1; num <= 100; num++) {
+ for(let num = start_num; num <= end_num; num++) {
      numbers.push(num)
  }
  return numbers
 }
 
-console.log(pineApple())
+console.log(pineApple(1,100))
 module.exports = pineApple
--- pineApple.test.js
+++ pineApple.test.js
@@ -1,6 +1,6 @@
 const pineApple = require('./pineApple')
 describe('pineApple', () => {
     it('returns numbers from 1 to 100', () => {
-        expect(pineApple().length).toEqual(100)
+        expect(pineApple(1,100).length).toEqual(100)
     })
 })
