e0f4c4b49367c8fc8c4d477aa54a233ec2e0f0e8
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-05-13 10:29:55 +0100

    Replaced toContain with toEqual

--- pineApple.js
+++ pineApple.js
@@ -1,11 +1,11 @@
 const pineApple = (start_num, end_num) => {
   const numbers = [];
   for (let num = start_num; num <= end_num; num++) {
-    if (isMultiple(num,3) && isMultiple(num, 5)) {
+    if (isMultiple(num, 3) && isMultiple(num, 5)) {
       numbers.push("PineApple");
-    } else if (isMultiple(num,3)) {
+    } else if (isMultiple(num, 3)) {
       numbers.push("Pine");
-    } else if (isMultiple(num,5)) {
+    } else if (isMultiple(num, 5)) {
       numbers.push("Apple");
     } else {
       numbers.push(num);
@@ -15,8 +15,8 @@ const pineApple = (start_num, end_num) => {
 };
 
 const isMultiple = (val, div) => {
-    return (val % div) === 0
-}
+  return val % div === 0;
+};
 
 console.log(pineApple(1, 100));
 module.exports = pineApple;
--- pineApple.test.js
+++ pineApple.test.js
@@ -1,18 +1,21 @@
 const pineApple = require('./pineApple')
 describe('pineApple', () => {
+    
     it('returns numbers from 1 to 100', () => {
         expect(pineApple(1,100).length).toEqual(100)
     })
 
     it('returns "Pine" instead of the number if number is multiple of 3', () => {
-        expect(pineApple(1,100)).toContain('Pine')
+        expect(pineApple(3,4)).toEqual(['Pine',4])
     })
 
     it('reurns Apple instead of number if number is multiple of 5', () => {
-        expect(pineApple(1,100)).toContain('Apple')
+        expect(pineApple(5,7)).toEqual(['Apple','Pine',7])
     })
 
     it('returns PineApple instead of number if number is multiple of both 3 and 5',() => {
-        expect(pineApple(1,100)).toContain('PineApple')
+        expect(pineApple(15,15)).toEqual(['PineApple'])
+        expect(pineApple(1,100)).toEqual(expect.arrayContaining([1,2,'Pine','Apple', 'PineApple', 98]))
+        expect(pineApple(1,100)).not.toEqual(expect.arrayContaining([3,5,15,30,90]))
     })
 })
